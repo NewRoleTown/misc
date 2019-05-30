c591c80f594c82a8e1a3bfa2d8586d2a5bcbe023
NewRoleTown <39359827+NewRoleTown@users.noreply.github.com>
2019-05-30 14:07:24 +0800

    读写锁
    读写锁

--- linux/arg/cas.c
+++ linux/arg/cas.c
@@ -46,3 +46,74 @@ int main(){
 	printf("count = %d\n",count);
 	return 0;
 }
+
+
+
+#if 0
+#include<stdio.h>
+#include<stdlib.h>
+
+#define rw_write_flag	(0x100000)
+
+struct rw_lock{
+	int count;
+};
+
+void init_rw_lock( struct rw_lock *pl ){
+	pl->count = rw_write_flag;
+	return;
+}
+
+int read_try_lock( struct rw_lock *pl ){
+
+	int old,new,ret;
+	do{
+		old = pl->count;
+		new = old - 1;
+		asm volatile("lock cmpxchg %2,%1":"=a"(ret):"m"(pl->count),"b"(new),"a"(old):);
+	}while( old != ret );
+
+	//fail
+	if( new >= 0 )
+		return 1;
+
+	asm volatile("lock incl %0"::"m"(pl->count):);
+	return 0;
+}
+
+void read_unlock( struct rw_lock *pl ){
+	asm volatile("lock incl %0"::"m"(pl->count):);
+	return;
+}
+
+int write_try_lock( struct rw_lock *pl ){
+	int old,new,ret;
+	do{
+		old = pl->count;
+		new = old - rw_write_flag;
+		asm volatile("lock cmpxchg %2,%1":"=a"(ret):"m"(pl->count),"b"(new),"a"(old):);
+	}while( old != ret );
+
+	if( !new )
+		return 1;
+
+	asm volatile("lock addl %0,%1"::"i"(rw_write_flag),"m"(pl->count):);
+	return 0;
+}
+
+int main(){
+	struct rw_lock lock;
+	init_rw_lock(&lock);
+
+	int ret = read_try_lock(&lock);
+	printf("%d\n",ret);
+	ret = read_try_lock(&lock);
+	printf("%d\n",ret);
+	ret = read_try_lock(&lock);
+	printf("%d\n",ret);
+	ret = write_try_lock(&lock);
+	printf("%d\n",ret);
+
+}
+
+#endif
